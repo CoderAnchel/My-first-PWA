46bce67f1a344adcb991d04450a4efe63e3aef9a
CoderAnchel <alex21fd@gmail.com>
2022-06-06 23:11:48 +0200

    Todo full bug pero 92, 100, 100, 100 algunas tarjestas desplegables subido en web

---
+++ main.js
@@ -0,0 +1,85 @@
+const shoes_card = document.querySelector(".shoes-card");
+const nft_card = document.querySelector(".nft-card-info");
+const s_card1 = document.querySelector(".card-1");
+const s_card2 = document.querySelector(".card-2");
+const s_card3 = document.querySelector(".card-3");
+const s_card4 = document.querySelector(".card-4");
+const s_card5 = document.querySelector(".card-5");
+const s_card6 = document.querySelector(".card-6");
+const s_card7 = document.querySelector(".card-7");
+const s_card8 = document.querySelector(".card-8");
+const s_card9 = document.querySelector(".card-9");
+const s_card10 = document.querySelector(".card-10");
+const s_card11 = document.querySelector(".card-11");
+const s_card12 = document.querySelector(".card-12");
+
+const close = document.querySelector(".close");
+
+s_card1.addEventListener("click", () => {
+  nft_card.classList.toggle("toggle");
+  close.classList.toggle("act");
+});
+
+s_card2.addEventListener("click", () => {
+  nft_card.classList.toggle("toggle");
+  close.classList.toggle("act");
+});
+
+s_card3.addEventListener("click", () => {
+  nft_card.classList.toggle("toggle");
+  close.classList.toggle("act");
+});
+
+s_card4.addEventListener("click", () => {
+  nft_card.classList.toggle("toggle");
+  close.classList.toggle("act");
+});
+
+s_card5.addEventListener("click", () => {
+  nft_card.classList.toggle("toggle");
+  close.classList.toggle("act");
+});
+
+s_card6.addEventListener("click", () => {
+  nft_card.classList.toggle("toggle");
+  close.classList.toggle("act");
+});
+
+s_card7.addEventListener("click", () => {
+  nft_card.classList.toggle("toggle");
+  close.classList.toggle("act");
+});
+
+s_card8.addEventListener("click", () => {
+  nft_card.classList.toggle("toggle");
+  close.classList.toggle("act");
+});
+
+s_card9.addEventListener("click", () => {
+  nft_card.classList.toggle("toggle");
+  close.classList.toggle("act");
+});
+
+s_card10.addEventListener("click", () => {
+  nft_card.classList.toggle("toggle");
+  close.classList.toggle("act");
+});
+
+s_card11.addEventListener("click", () => {
+  nft_card.classList.toggle("toggle");
+  close.classList.toggle("act");
+});
+
+s_card12.addEventListener("click", () => {
+  nft_card.classList.toggle("toggle");
+  close.classList.toggle("act");
+});
+
+close.addEventListener("click", () => {
+  nft_card.classList.toggle("toggle");
+  close.classList.toggle("dissa");
+});
+
+nft_card.addEventListener("click", () =>{
+    nft_card.classList.toggle("dess");
+})
